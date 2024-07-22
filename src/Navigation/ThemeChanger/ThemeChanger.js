9576b46b149c9e36fc5995367f977e21c74d313c
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-07-22 13:19:03 +0300

    add label to icon

--- src/Navigation/ThemeChanger/ThemeChanger.js
+++ src/Navigation/ThemeChanger/ThemeChanger.js
@@ -2,15 +2,20 @@ import { useContext } from "react";
 import { ThemeContext } from "../../Resources/Theme/ThemeContext"
 import { darkTheme, lightTheme } from "../../Resources/Theme/Theme"
 import TypographyWithTheme from "../../Resources/StyledComponents/TypographyWithTheme";
+import GridItemAligned from "../../Resources/StyledComponents/GridItemAligned";
+import { Grid } from "@mui/material";
 
 const ThemeChanger = () => {
     const { currentTheme, setCurrentTheme } = useContext(ThemeContext);
     return (
-        <>
-            <img src={currentTheme.images.navBarImage} alt="Logo" style={{ cursor: "pointer", width: "auto", height: "8vh" }}
-                onClick={() => currentTheme === darkTheme ? setCurrentTheme(lightTheme) : setCurrentTheme(darkTheme)} />
-            <TypographyWithTheme color={currentTheme.colors.textColor}>Change theme</TypographyWithTheme>
-        </>)
+        <GridItemAligned container onClick={() => currentTheme === darkTheme ? setCurrentTheme(lightTheme) : setCurrentTheme(darkTheme)} style={{ cursor: "pointer"}}>
+            <Grid item xs={6} style={{ float: 'right', marginLeft: 'auto', textAlign: 'right' }}>
+                <img src={currentTheme.images.navBarImage} alt="Logo" style={{ width: "auto", height: "8vh" }}/>
+            </Grid>
+            <Grid item xs={6}>
+                <TypographyWithTheme color={currentTheme.colors.textColor}>Change theme</TypographyWithTheme>
+            </Grid>
+        </GridItemAligned>)
 }
 
 export default ThemeChanger;
