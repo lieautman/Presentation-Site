52f23b90153077263d33c8404bcd47d3f3cc7752
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-07-22 13:03:12 +0300

    change image label

--- src/Navigation/ThemeChanger/ThemeChanger.js
+++ src/Navigation/ThemeChanger/ThemeChanger.js
@@ -1,11 +1,16 @@
 import { useContext } from "react";
 import { ThemeContext } from "../../Resources/Theme/ThemeContext"
 import { darkTheme, lightTheme } from "../../Resources/Theme/Theme"
+import TypographyWithTheme from "../../Resources/StyledComponents/TypographyWithTheme";
 
 const ThemeChanger = () => {
     const { currentTheme, setCurrentTheme } = useContext(ThemeContext);
-    return <img src={currentTheme.images.navBarImage} alt="Logo" style={{ cursor: "pointer", width: "auto", height: "8vh" }}
-        onClick={()=>currentTheme===darkTheme?setCurrentTheme(lightTheme):setCurrentTheme(darkTheme)} />
+    return (
+        <>
+            <img src={currentTheme.images.navBarImage} alt="Logo" style={{ cursor: "pointer", width: "auto", height: "8vh" }}
+                onClick={() => currentTheme === darkTheme ? setCurrentTheme(lightTheme) : setCurrentTheme(darkTheme)} />
+            <TypographyWithTheme color={currentTheme.colors.textColor}>Change theme</TypographyWithTheme>
+        </>)
 }
 
 export default ThemeChanger;
