0381aafa04b0ed301af281c693d45aef41c67d48
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-07-23 22:52:43 +0300

    add navbar translation

--- src/Navigation/NavigationComponent.js
+++ src/Navigation/NavigationComponent.js
@@ -6,17 +6,19 @@ import TypographyWithTheme from "../Resources/StyledComponents/TypographyWithThe
 import { Button } from "@mui/material";
 import ThemeChanger from "./ThemeChanger/ThemeChanger";
 import LanguageChanger from "./LanguageChanger/LanguageChanger";
+import { useTranslation } from 'react-i18next';
 
 function NavigationComponent(porps) {
+    const { t } = useTranslation();
     const { currentTheme } = useContext(ThemeContext);
     return (
         <GridContainerWithThemeMainColor container className="scrollSnapSection">
             <GridItemAligned item xs={2}><ThemeChanger /></GridItemAligned>
             <GridItemAligned item xs={2}><LanguageChanger /></GridItemAligned>
-            <GridItemAligned item xs={2}><Button onClick={() => porps.setPageNo(1)}><TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>Acasa</h3></TypographyWithTheme></Button></GridItemAligned>
-            <GridItemAligned item xs={2}><Button onClick={() => porps.setPageNo(2)}><TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>Despre</h3></TypographyWithTheme></Button></GridItemAligned>
-            <GridItemAligned item xs={2}><Button onClick={() => porps.setPageNo(3)}><TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>Contact</h3></TypographyWithTheme></Button></GridItemAligned>
-            <GridItemAligned item xs={2}><Button onClick={() => porps.setPageNo(4)}><TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>CV</h3></TypographyWithTheme></Button></GridItemAligned>
+            <GridItemAligned item xs={2}><Button onClick={() => porps.setPageNo(1)}><TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>{t("Nav.Pages.Home")}</h3></TypographyWithTheme></Button></GridItemAligned>
+            <GridItemAligned item xs={2}><Button onClick={() => porps.setPageNo(2)}><TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>{t("Nav.Pages.About")}</h3></TypographyWithTheme></Button></GridItemAligned>
+            <GridItemAligned item xs={2}><Button onClick={() => porps.setPageNo(3)}><TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>{t("Nav.Pages.Contact")}</h3></TypographyWithTheme></Button></GridItemAligned>
+            <GridItemAligned item xs={2}><Button onClick={() => porps.setPageNo(4)}><TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>{t("Nav.Pages.Resume")}</h3></TypographyWithTheme></Button></GridItemAligned>
         </GridContainerWithThemeMainColor>
     )
 }
--- src/Navigation/ThemeChanger/ThemeChanger.js
+++ src/Navigation/ThemeChanger/ThemeChanger.js
@@ -4,8 +4,10 @@ import { darkTheme, lightTheme } from "../../Resources/Theme/Theme"
 import TypographyWithTheme from "../../Resources/StyledComponents/TypographyWithTheme";
 import GridItemAligned from "../../Resources/StyledComponents/GridItemAligned";
 import { Grid } from "@mui/material";
+import { useTranslation } from 'react-i18next';
 
 const ThemeChanger = () => {
+    const { t } = useTranslation();
     const { currentTheme, setCurrentTheme } = useContext(ThemeContext);
     return (
         <GridItemAligned container onClick={() => currentTheme === darkTheme ? setCurrentTheme(lightTheme) : setCurrentTheme(darkTheme)} style={{ cursor: "pointer"}}>
@@ -13,7 +15,7 @@ const ThemeChanger = () => {
                 <img src={currentTheme.images.navBarThemeImage} alt="Logo" style={{ width: "auto", height: "8vh" }}/>
             </Grid>
             <Grid item xs={6}>
-                <TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>Change theme</h3></TypographyWithTheme>
+                <TypographyWithTheme color={currentTheme.colors.textColor}><h3 style={{fontSize:currentTheme.fontSizes.h3}}>{t("Nav.ChangeTheme.label")}</h3></TypographyWithTheme>
             </Grid>
         </GridItemAligned>)
 }
