4ff7932ffe580e106077a37010f010432bc4bb67
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-07-25 01:38:53 +0300

    add country colors

--- src/Resources/StyledComponents/ScrollbarWrapper.js
+++ src/Resources/StyledComponents/ScrollbarWrapper.js
@@ -1,15 +1,23 @@
 import { styled } from '@mui/material';
 import Box from '@mui/material/Grid';
+import { useTranslation } from 'react-i18next';
 
-const ScrollbarWrapper = styled(Box)`
+const ScrollbarWrapper = (props) => {
+    const { i18n } = useTranslation();
+
+    const ScrollbarWrapperStyled = styled(Box)`
     height: 100vh;
     overflow-Y: scroll;
     &::-webkit-scrollbar {
         width: 8px;
     };
     &::-webkit-scrollbar-thumb {
-        background: linear-gradient(90deg,red, white, blue);
+        background: linear-gradient(90deg,${i18n.language == "en" ? "red, white, blue" : "red, yellow, blue"});
     }
-`;
+    `;
+    return <ScrollbarWrapperStyled {...props} >{props.children}</ScrollbarWrapperStyled>
+}
+
+
 
 export default ScrollbarWrapper;
