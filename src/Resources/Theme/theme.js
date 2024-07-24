dca7c339f9aa9fc417de8e02a1930d4e5611cd9d
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-07-25 01:03:36 +0300

    color switch

--- src/Resources/Theme/theme.js
+++ src/Resources/Theme/theme.js
@@ -1,5 +1,5 @@
 import { createTheme } from '@mui/material/styles';
-import darkBackgroundHome from "../../Resources/Images/darkBackgroundHome1.jpg"
+import darkBackgroundHome from "../../Resources/Images/darkBackgroundHome.jpg"
 import lightBackgroundHome from "../../Resources/Images/lightBackgroundHome.jpg"
 import navBarDark from "../../Resources/Images/logoDark.png"
 import navBarLight from "../../Resources/Images/logoLight.png"
@@ -7,8 +7,8 @@ import navBarLight from "../../Resources/Images/logoLight.png"
 export const darkTheme = createTheme({
     colors: {
         primaryColor: 'rgba(92, 44, 95, 1)',
-        secondaryColor: 'rgba(7, 17, 42, 1)',
-        primaryAccentsColor: 'rgba(129, 110, 27, 1)',
+        secondaryColor: 'rgba(35, 35, 35, 1)',
+        primaryAccentsColor: 'rgba(90, 90, 117, 1)',
         secondaryAccentsColor: 'rgba(105, 32, 32, 1)',
         textColor: 'white',
         titleColor: 'rgba(174, 109, 210, 1)',
@@ -29,8 +29,8 @@ export const darkTheme = createTheme({
 export const lightTheme = createTheme({
     colors: {
         primaryColor: 'rgba(79, 115, 108, 1)',
-        secondaryColor: 'rgba(128, 103, 28, 1)',
-        primaryAccentsColor: 'rgba(37, 90, 99, 1)',
+        secondaryColor: 'rgba(119, 152, 121, 1)',
+        primaryAccentsColor: 'rgba(45, 45, 45, 1)',
         secondaryAccentsColor: 'rgba(150, 223, 223, 1)',
         textColor: 'black',
         titleColor: 'rgba(56, 17, 74, 1)',
