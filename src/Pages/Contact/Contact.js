9ff39e09103da7f0c25e16e4fe202daa24c5de58
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-07-26 00:44:24 +0300

    COntact page start

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,7 +1,74 @@
+import BoxWithvh from "../../Resources/StyledComponents/BoxWithvh";
+import TypographyWithTheme from "../../Resources/StyledComponents/TypographyWithTheme";
+import { useContext } from "react";
+import { ThemeContext } from "../../Resources/Theme/ThemeContext"
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTranslation } from 'react-i18next';
+import { Box, IconButton } from '@mui/material';
+import { AssignmentInd } from '@mui/icons-material';
+
+
 function Contact() {
+    const { t, i18n } = useTranslation();
+    const { currentTheme } = useContext(ThemeContext);
+    const matches = useMediaQuery('(min-width:600px)');
 
     return (
-        <>Contact</>
+        <>
+            <BoxWithvh
+                height={91}
+                sx={{
+                    backgroundImage: `url(${currentTheme.images.resumeFirstImage})`,
+                    backgroundSize: matches ? "100% 100%" : "150% 100%",
+                    backgroundPosition: 'center',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    textAlign: "center"
+                }}>
+                <TypographyWithTheme color={currentTheme.colors.titleColor}>
+                    <h1 style={{ fontSize: matches ? currentTheme.fontSizes.h1 : currentTheme.fontSizesPhone.h1 }}>{t("Contact.Page1.Title1")}</h1>
+                </TypographyWithTheme>
+                <TypographyWithTheme color={currentTheme.colors.titleColor}>
+                    <h2 style={{ fontSize: matches ? currentTheme.fontSizes.h2 : currentTheme.fontSizesPhone.h2 }}>{t("Contact.Page1.Title2")}</h2>
+                </TypographyWithTheme>
+            </BoxWithvh >
+            <BoxWithvh
+                height={100}
+                className="scrollSnapSection"
+                backgroundColor={currentTheme.colors.primaryAccentsColor}
+            >
+                <BoxWithvh
+                    height={10}
+                    backgroundColor={currentTheme.colors.secondaryColor}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        textAlign: "center"
+                    }}
+                >
+                    <TypographyWithTheme color={currentTheme.colors.titleColor}>
+                        <h1 style={{ fontSize: matches ? currentTheme.fontSizes.h1 : currentTheme.fontSizesPhone.h1 }}>{t("Contact.Page2.Title1")}</h1>
+                    </TypographyWithTheme>
+                </BoxWithvh>
+                <BoxWithvh
+                    height={40}
+                    backgroundColor={currentTheme.colors.secondaryColor}
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        marginTop: '25vh',
+                        textAlign: "center"
+                    }}
+                >
+                </BoxWithvh>
+            </BoxWithvh>
+        </>
     )
 }
 
