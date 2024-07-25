095f70e84282a43e368d236b5c0b35b815fc61c6
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-07-26 01:04:53 +0300

    email service install

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,17 +1,44 @@
 import BoxWithvh from "../../Resources/StyledComponents/BoxWithvh";
 import TypographyWithTheme from "../../Resources/StyledComponents/TypographyWithTheme";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { ThemeContext } from "../../Resources/Theme/ThemeContext"
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTranslation } from 'react-i18next';
-import { Box, IconButton } from '@mui/material';
-import { AssignmentInd } from '@mui/icons-material';
+import emailjs from "@emailjs/browser";
 
 
 function Contact() {
-    const { t, i18n } = useTranslation();
+    const { t } = useTranslation();
     const { currentTheme } = useContext(ThemeContext);
     const matches = useMediaQuery('(min-width:600px)');
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const ServiceId = "service_qa9ig88";
+        const TemplateId = "template_gkk4c6b";
+        const PublicKey = "IuBo1lXrKMjYBbSKG";
+
+        const templateParams = {
+            from_name: name,
+            from_email: email,
+            to_name: "Cristi",
+            message: message
+        };
+
+        emailjs.send(ServiceId, TemplateId, templateParams, PublicKey).then((response) => {
+            console.log("Email sent succesfully!", response);
+            setName("");
+            setEmail("");
+            setMessage("");
+        }).catch((error) => {
+            console.error("Error sending email:", error)
+        })
+    }
 
     return (
         <>
@@ -66,6 +93,12 @@ function Contact() {
                         textAlign: "center"
                     }}
                 >
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Your name" value={name} onChange={(e) => setName(e.target.value)} />
+                        <input type="text" placeholder="Your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <textarea cols="30" rows="30" value={message} onChange={(e) => setMessage(e.target.value)} />
+                        <button type="submit">Send email</button>
+                    </form>
                 </BoxWithvh>
             </BoxWithvh>
         </>
