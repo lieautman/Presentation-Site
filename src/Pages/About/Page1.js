eac93eba89edc0d8e78414458b418c09f3745ff8
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-07-31 23:22:44 +0300

    add AboutPage

---
+++ src/Pages/About/Page1.js
@@ -0,0 +1,40 @@
+import TypographyWithTheme from "../../Resources/StyledComponents/TypographyWithTheme"
+import { useContext } from "react"
+import { ThemeContext } from "../../Resources/Theme/ThemeContext"
+import useMediaQuery from "@mui/material/useMediaQuery"
+import { useTranslation } from "react-i18next"
+import Box from "@mui/material/Box"
+
+function Page1() {
+  const { t } = useTranslation()
+  const { currentTheme } = useContext(ThemeContext)
+  const matches = useMediaQuery("(min-width:600px)")
+
+  return (
+    <>
+      <Box
+        sx={{
+          backgroundImage: `url(${currentTheme.images.contactFirstImage})`,
+          backgroundSize: matches ? "100% 100%" : "150% 100%",
+          backgroundPosition: "center",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          textAlign: "center",
+          height: "91vh",
+        }}>
+        <TypographyWithTheme color={currentTheme.colors.titleColor}>
+          <h1
+            style={{
+              fontSize: matches ? currentTheme.fontSizes.h1 : currentTheme.fontSizesPhone.h1,
+            }}>
+            {t("About.Page1.Title1")}
+          </h1>
+        </TypographyWithTheme>
+      </Box>
+    </>
+  )
+}
+
+export default Page1
