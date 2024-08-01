e799b6263ff828ad25b8a033cdb77d7517429ebf
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-08-01 23:52:55 +0300

    css

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,7 +1,7 @@
 import Page1 from "./Page1"
 import Page2 from "./Page2"
 import Page3 from "./Page3"
-import Page4 from "./Page4"
+//import Page4 from "./Page4"
 
 function About() {
   return (
@@ -9,7 +9,6 @@ function About() {
       <Page1 />
       <Page2 />
       <Page3 />
-      <Page4 />
     </>
   )
 }
