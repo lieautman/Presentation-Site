40ac68885c47f61f18dd045198fa4ef1ed0cbd3f
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-08-05 21:04:30 +0300

    coment unused resources

--- src/Resources/Theme/theme.js
+++ src/Resources/Theme/theme.js
@@ -1,14 +1,14 @@
 import { createTheme } from "@mui/material/styles"
-import darkBackgroundHome from "../../Resources/Images/darkBackgroundHome.jpg"
-import lightBackgroundHome from "../../Resources/Images/lightBackgroundHome.jpg"
-import darkBackgroundAbout from "../../Resources/Images/darkBackgroundAbout.jpg"
-import lightBackgroundAbout from "../../Resources/Images/lightBackgroundAbout.jpg"
-import darkBackgroundContact from "../../Resources/Images/darkBackgroundContact.jpg"
-import lightBackgroundContact from "../../Resources/Images/lightBackgroundContact.jpg"
-import darkBackgroundResume from "../../Resources/Images/darkBackgroundResume.jpg"
-import lightBackgroundResume from "../../Resources/Images/lightBackgroundResume.jpg"
+// import darkBackgroundHome from "../../Resources/Images/darkBackgroundHome.jpg"
+// import lightBackgroundHome from "../../Resources/Images/lightBackgroundHome.jpg"
+// import darkBackgroundAbout from "../../Resources/Images/darkBackgroundAbout.jpg"
+// import lightBackgroundAbout from "../../Resources/Images/lightBackgroundAbout.jpg"
+// import darkBackgroundContact from "../../Resources/Images/darkBackgroundContact.jpg"
+// import lightBackgroundContact from "../../Resources/Images/lightBackgroundContact.jpg"
+// import darkBackgroundResume from "../../Resources/Images/darkBackgroundResume.jpg"
+// import lightBackgroundResume from "../../Resources/Images/lightBackgroundResume.jpg"
 import navBarDark from "../../Resources/Images/logoDark.png"
-import navBarLight from "../../Resources/Images/logoLight.png"
+//import navBarLight from "../../Resources/Images/logoLight.png"
 
 export const darkTheme = createTheme({
   colors: {
@@ -54,39 +54,6 @@ export const simpleTheme = createTheme({
   },
   images: {
     navBarThemeImage: navBarDark,
-  },
-  fontSizes: {
-    h1Big: "5vmin",
-    h1: "4vmin",
-    h2: "3vmin",
-    h3: "2vmin",
-    p: "1vmin",
-  },
-  fontSizesPhone: {
-    h1Big: "8vmin",
-    h1: "6vmin",
-    h2: "4vmin",
-    h3: "3vmin",
-    p: "2vmin",
-  },
-  logoSize: "8vh",
-})
-
-export const lightTheme = createTheme({
-  colors: {
-    primaryColor: "rgba(79, 115, 108, 1)",
-    secondaryColor: "rgba(119, 152, 121, 1)",
-    primaryAccentsColor: "rgba(45, 45, 45, 1)",
-    secondaryAccentsColor: "rgba(150, 223, 223, 1)",
-    textOnDark: "black",
-    textOnLight: "rgba(56, 17, 74, 1)",
-  },
-  images: {
-    navBarThemeImage: navBarLight,
-    homeFirstImage: lightBackgroundHome,
-    aboutFirstImage: lightBackgroundAbout,
-    contactFirstImage: lightBackgroundContact,
-    resumeFirstImage: lightBackgroundResume,
   },
   fontSizes: {
     h1Big: "5vmin",
