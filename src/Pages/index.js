e7fc833546c536e00450c49ec457ff8895055b44
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-08-21 23:43:58 +0300

    add not found

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -2,6 +2,7 @@ import Home from "./Home"
 import About from "./About"
 import Contact from "./Contact"
 import Resume from "./Resume"
+import NoPageFound from "./NoPageFound"
 import { Routes, Route, HashRouter } from "react-router-dom"
 
 function Pages() {
@@ -13,6 +14,7 @@ function Pages() {
         <Route exact path="/about" element={<About />} />
         <Route exact path="/contact" element={<Contact />} />
         <Route exact path="/resume" element={<Resume />} />
+        <Route path="/*" element={<NoPageFound />} />
       </Routes>
     </HashRouter>
   )
